bd3b1221e90c7eea944985e27144eb58cdb31a08
Kunal Buch <kbbuch@ncsu.edu>
2017-10-21 02:00:19 -0400

    top modified for synthesis!

--- top.v
+++ top.v
@@ -1,24 +1,29 @@
-module top(input size, input_char, clk, reset,
-             output input_char_flag);
+module top	(input input_char_2, input_char, clk, reset, 
+			 output input_char_flag, accepting_match_flag, 
+				accepting_match_flag_2, i);
 
 	wire [19:0] tb_addr;
 	wire [31:0] tb_rd_bus;
 	
-	wire        input_char_flag;
-	
+	parameter size_range = 9514;
 	
 	design_1_wrapper B_Mem
 	   (.BRAM_PORTA_addr(tb_addr),
 		.BRAM_PORTA_clk(clk),
 		.BRAM_PORTA_dout(tb_rd_bus));
 		
-	CSR_traversal C1 
-		(.clk(clk),
+	CSR_traversal #(.size_range(size_range)) C1 
+		(.clk(tb_clk),
 		.reset(reset),
-		.size(size), 
+		.size(size_range), 
 		.rd_address(tb_addr), 
 		.rd_bus(tb_rd_bus), 
 		.input_char_flag(input_char_flag),
-		.input_char(input_char));
+		.input_char(input_char),
+		.input_char_2(input_char_2),
+		.i(i),
+		.accepting_match_flag(accepting_match_flag),
+		.accepting_match_flag_2(accepting_match_flag_2)
+		);
 		
 endmodule
